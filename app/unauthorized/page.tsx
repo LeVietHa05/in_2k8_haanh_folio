190a701f28832cb3c8ae288516c657db8f95139b
LeVietHa05 <ha.leviet.0504@gmail.com>
2025-04-06 15:17:49 +0700

    Wrap NoAuthorized component in Suspense for improved loading handling

--- app/unauthorized/page.tsx
+++ app/unauthorized/page.tsx
@@ -1,5 +1,9 @@
 import NoAuthorized from "../components/unAuthorized";
-
+import { Suspense } from "react";
 export default function Unauthorized() {
-  return <NoAuthorized />;
+  return (
+    <Suspense>
+      <NoAuthorized />
+    </Suspense>
+  );
 }
