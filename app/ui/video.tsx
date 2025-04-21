d6fcfbff7e70b93c0431f9905f6f11169912ff60
LeVietHa05 <ha.leviet.0504@gmail.com>
2025-04-21 16:33:19 +0700

    Add ScrollToTop component, update Video component dimensions and behavior, and replace placeholder images for improved user experience

--- app/ui/video.tsx
+++ app/ui/video.tsx
@@ -1,6 +1,6 @@
 export function Video({ _link }: { _link: string }) {
   return (
-    <video width="1120" height="371" controls autoPlay={true} preload="none">
+    <video width="338" height="600"  autoPlay={true} loop={true} muted>
       <source src={_link} type="video/mp4" />
       Your browser does not support the video tag.
     </video>
