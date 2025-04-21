8b85f2d7c02627ffbc33fd65fdac4638fae405d8
LeVietHa05 <ha.leviet.0504@gmail.com>
2025-04-21 18:28:56 +0700

    responsive for charity

--- app/charity/page.tsx
+++ app/charity/page.tsx
@@ -3,12 +3,12 @@ import Link from "next/link";
 export default function Home() {
   return (
     <div className="background-container pb-8 text-gray-50">
-      <div className="max-w-[1440px] w-full mx-auto px-20">
+      <div className="sm:max-w-[1440px] w-full mx-auto sm:px-20 px-3">
         <main>
           {/* main here */}
-          <div className="pt-20 text-gray-700 text-2xl leading-[36px]">
-            <div className="bg-white/30 backdrop-blur-sm p-10 flex flex-col items-center rounded-lg">
-              <div className="max-w-[900px] mx-auto ">
+          <div className="sm:pt-20 text-gray-700 text-base sm:text-2xl leading-[30px] sm:leading-[36px]">
+            <div className="bg-white/30 backdrop-blur-sm sm:p-10 flex flex-col items-center rounded-lg">
+              <div className="sm:max-w-[900px] w-full mx-auto ">
                 Most of the proceeds from the sale of pottery art pieces are
                 used to fund environmental and charitable projects founded by
                 Nguyễn Hà Anh. These philanthropic organizations and projects
@@ -18,9 +18,9 @@ export default function Home() {
                 <br />
                 LotusMed Vietnam (Prescreening thyroid cancer for women). <br />
                 <Link
-                  className="underline text-[#518224]"
+                  className="underline text-[#518224] w-full"
                   href={"https://www.facebook.com/lotusmedvietnam"}>
-                  https://www.facebook.com/lotusmedvietnam
+                  https://www.facebook .com/lotusmedvietnam
                 </Link>
               </div>
               <div className="py-10">
@@ -32,8 +32,8 @@ export default function Home() {
               </div>
             </div>
 
-            <div className="bg-white/30 backdrop-blur-sm mt-10 p-10 flex flex-col items-center rounded-lg">
-              <div className="max-w-[900px] mx-auto pt-20 ">
+            <div className="bg-white/30 backdrop-blur-sm mt-10 sm:p-10 flex flex-col items-center rounded-lg">
+              <div className="sm:max-w-[900px] w-full mx-auto sm:pt-20 pt-4">
                 Founded in 2021, Eco Wander is a non-profit organization
                 established by students in Vietnam with the mission of raising
                 public awareness about environmental protection, focusing on
@@ -44,10 +44,10 @@ export default function Home() {
                 <Link
                   className="underline text-[#518224]"
                   href={"https://www.facebook.com/eco.wander24"}>
-                  https://www.facebook.com/eco.wander24
+                  https://www.facebook .com/eco.wander24
                 </Link>
               </div>
-              <div className="py-10 flex gap-10 justify-between shrink">
+              <div className="py-10 flex flex-col sm:flex-row gap-3 sm:gap-10 justify-between shrink">
                 <div className="shrink">
                   <Image
                     src={"/charity/2.png"}
