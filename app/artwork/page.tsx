54aa9e98cd125e003791f90d7f2d2f8677575a44
LeVietHa05 <ha.leviet.0504@gmail.com>
2025-04-21 16:33:07 +0700

    Refactor Home component to remove background rotation logic, enhance image links for auction items, and update styles for improved presentation; add new artwork images.

--- app/artwork/page.tsx
+++ app/artwork/page.tsx
@@ -1,156 +1,81 @@
-"use client";
-
-import { useState, useEffect } from "react";
-
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
-  // background change
-  const backgrounds = ["/bg-1.png", "/bg-2.png", "/bg-3.png"];
-
-  const [currentBackground, setCurrentBackground] = useState(0);
-
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setCurrentBackground((prev) => (prev + 1) % backgrounds.length);
-    }, 5000); // Chuyển đổi mỗi 5 giây
-
-    return () => clearInterval(interval); // Dọn dẹp interval khi component unmount
-  }, [backgrounds.length]);
-
   // popup and image
   const imgOnSaleLink = [
     {
       id: 1,
       src: "/artwork/1.png",
+      link: "/auction#artwork1",
       title: "lorem 1",
       info: " Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nostrum consequuntur cupiditate aut perspiciatis est debitis rerum quidem aperiam dolor, fugiat officia ea tempore labore nisi nihil iure eum totam officiis!",
     },
     {
       id: 2,
       src: "/artwork/2.png",
+      link: "/auction#artwork2",
       title: "lorem 2",
       info: " Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nostrum consequuntur cupiditate aut perspiciatis est debitis rerum quidem aperiam dolor, fugiat officia ea tempore labore nisi nihil iure eum totam officiis!",
     },
     {
       id: 3,
       src: "/artwork/3.png",
+      link: "/auction#artwork3",
       title: "lorem 3",
       info: " Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nostrum consequuntur cupiditate aut perspiciatis est debitis rerum quidem aperiam dolor, fugiat officia ea tempore labore nisi nihil iure eum totam officiis!",
     },
     {
       id: 4,
       src: "/artwork/4.png",
+      link: "/auction#artwork4",
+      title: "lorem 4",
+      info: " Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nostrum consequuntur cupiditate aut perspiciatis est debitis rerum quidem aperiam dolor, fugiat officia ea tempore labore nisi nihil iure eum totam officiis!",
+    },
+    {
+      id: 5,
+      src: "/artwork/5.png",
+      link: "/auction#artwork5",
+      title: "lorem 4",
+      info: " Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nostrum consequuntur cupiditate aut perspiciatis est debitis rerum quidem aperiam dolor, fugiat officia ea tempore labore nisi nihil iure eum totam officiis!",
+    },
+    {
+      id: 6,
+      src: "/artwork/6.png",
+      link: "/auction#artwork6",
       title: "lorem 4",
       info: " Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nostrum consequuntur cupiditate aut perspiciatis est debitis rerum quidem aperiam dolor, fugiat officia ea tempore labore nisi nihil iure eum totam officiis!",
     },
   ];
 
-  interface IMAGEINFO {
-    id: number;
-    src: string;
-    title: string;
-    info: string;
-  }
-  const [selectedImage, setSelectedImage] = useState<IMAGEINFO | null>(null);
-
-  const openPopup = (image: IMAGEINFO) => {
-    setSelectedImage(image);
-  };
-
-  const closePopup = () => {
-    setSelectedImage(null);
-  };
-
   return (
-    <div
-      className="background-container pb-8 text-gray-50 min-h-[100vh]"
-      style={{
-        backgroundImage: `url(${backgrounds[currentBackground]})`,
-        transition: "background-image 1s ease-in-out", // Hiệu ứng chuyển đổi mượt mà
-      }}>
-      <div className="max-w-[1440px] mx-auto px-15">
-        <main className="pt-[200px]">
+    <div className="background-container pb-8 text-gray-50 pt-10">
+      <div className="max-w-[1440px] mx-auto px-15 ">
+        <main className=" backdrop-blur-sm bg-white/30">
           {/* main here */}
-          <div className="vogue uppercase text-center text-[44px] leading-[40px] py-4">
+          <div className="vogue uppercase text-[#518224] text-center text-[44px] leading-[40px] py-4">
             my artwork
           </div>
-          <div className="flex gap-10 justify-between py-10 border-y-1 border-white/30">
+          <div className="grid grid-cols-3 gap-6 py-10 border-y-1 border-white/30 *:w-[280px] *:m-auto ">
             {imgOnSaleLink.map((image) => {
               return (
-                <div key={image.id} className="relative cursor-pointer">
-                  <Image
-                    src={image.src}
-                    width={280}
-                    height={304}
-                    alt="anh san pham"
-                    onClick={() => {
-                      openPopup(image);
-                    }}
-                    className="hover:scale-110"
-                  />
-                </div>
+                <Link key={image.id} href={image.link} scroll={false}>
+                  <div className="flex justify-between items-center flex-col relative cursor-pointer group *:transition *:duration-300">
+                    <Image
+                      src={image.src}
+                      width={280}
+                      height={304}
+                      alt="anh san pham"
+                      className="group-hover:scale-125 "
+                    />
+                    <div className="text-[#40651D] font-semibold text-[22px] opacity-0 group-hover:opacity-100 absolute bottom-0 right-0 left-0 text-center backdrop-blur-sm py-2 bg-white/30">
+                      Click here for PRICE/AUTION
+                    </div>
+                  </div>
+                </Link>
               );
             })}
           </div>
-
-          {selectedImage && (
-            <div
-              className="fixed top-0 left-0 right-0 bottom-0 bg-white/50 flex justify-center items-center z-1"
-              style={{
-                animation: "fadeIn 1s ease-in-out",
-              }}>
-              <div
-                className="w-[1080px]  relative rounded-2xl bg-linear-45 from-[#101828] to-[#475467] "
-                style={{ animation: " slideIn 1s ease-in-out" }}>
-                <button
-                  className="transition-all hover:scale-110 hover:rotate-90 p-4 absolute right-0 top-0"
-                  onClick={closePopup}>
-                  <Image
-                    src={"/icons/Close.svg"}
-                    width={24}
-                    height={24}
-                    alt="close"></Image>
-                </button>
-
-                <div className="flex gap-10">
-                  <Image
-                    src={selectedImage.src}
-                    width={280}
-                    height={304}
-                    alt="Popup Image"
-                    className=""
-                  />
-                  <div className="flex flex-col justify-center">
-                    <div className="vogue text-[28px]">
-                      {selectedImage.title}
-                    </div>
-                    <p className="text-base leading-8">{selectedImage.info}</p>
-                    <div className="flex gap-10 items-center pt-5">
-                      <div className="flex flex-col bg-linear-45 from-[#42307D] to-[#7F56D9] px-2 py-4 rounded-[12px] h-fit">
-                        <div className="text-2xl leading-9 font-semibold">
-                          Call for price
-                        </div>
-                        <div className="flex gap-2">
-                          <Image
-                            src={"/icons/_WhatsApp.png"}
-                            width={24}
-                            height={24}
-                            alt="whatsapp"></Image>
-                          <div>+848 577 81385</div>
-                        </div>
-                      </div>
-                      <Image
-                        src={"/icons/onSale.png"}
-                        width={116}
-                        height={133}
-                        alt="onsale"></Image>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          )}
         </main>
       </div>
     </div>
