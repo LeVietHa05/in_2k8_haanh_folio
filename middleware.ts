f0011f737a3b0de4899d4e0456b82f27f2f80dd3
LeVietHa05 <ha.leviet.0504@gmail.com>
2025-05-11 20:49:30 +0700

    go to production

--- middleware.ts
+++ middleware.ts
@@ -4,25 +4,25 @@ import type { NextRequest } from "next/server";
 const VALID_SPECIAL_KEY = "development";
 
 export function middleware(request: NextRequest) {
-  const url = request.nextUrl;
-  const specialKey = url.searchParams.get("specialKey");
-
-  // Debugging: Log để kiểm tra specialKey
-  console.log("specialKey in middleware:", specialKey);
-
-  // Nếu specialKey không hợp lệ, chuyển hướng đến trang unauthorized
-  if (specialKey !== VALID_SPECIAL_KEY) {
-    // Lấy URL gốc mà người dùng đang cố truy cập
-    const redirectUrl = url.pathname + url.search;
-    // Chuyển hướng đến /unauthorized với redirectUrl và invalidKey
-    const redirectTo = new URL("/unauthorized", request.url);
-    redirectTo.searchParams.set("redirect", redirectUrl);
-    // Nếu có specialKey và nó không hợp lệ, thêm invalidKey=true
-    if (specialKey) {
-      redirectTo.searchParams.set("invalidKey", "true");
-    }
-    return NextResponse.redirect(redirectTo);
-  }
+//   const url = request.nextUrl;
+//   const specialKey = url.searchParams.get("specialKey");
+// 
+//   // Debugging: Log để kiểm tra specialKey
+//   console.log("specialKey in middleware:", specialKey);
+// 
+//   // Nếu specialKey không hợp lệ, chuyển hướng đến trang unauthorized
+//   if (specialKey !== VALID_SPECIAL_KEY) {
+//     // Lấy URL gốc mà người dùng đang cố truy cập
+//     const redirectUrl = url.pathname + url.search;
+//     // Chuyển hướng đến /unauthorized với redirectUrl và invalidKey
+//     const redirectTo = new URL("/unauthorized", request.url);
+//     redirectTo.searchParams.set("redirect", redirectUrl);
+//     // Nếu có specialKey và nó không hợp lệ, thêm invalidKey=true
+//     if (specialKey) {
+//       redirectTo.searchParams.set("invalidKey", "true");
+//     }
+//     return NextResponse.redirect(redirectTo);
+//   }
 
   // Nếu hợp lệ, tiếp tục render trang
   return NextResponse.next();
